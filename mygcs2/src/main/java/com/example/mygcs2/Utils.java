1cb95b6a25045fe882efd9f9668f2de524e489e5
HeeHoon Kim <fnaticisop@gmail.com>
2020-07-20 11:12:28 +0900

    arrowhead 추가

--- mygcs2/src/main/java/com/example/mygcs2/Utils.java
+++ mygcs2/src/main/java/com/example/mygcs2/Utils.java
@@ -4,13 +4,11 @@ import com.naver.maps.geometry.LatLng;
 
 public class Utils {
     public static LatLng headPointer(LatLng currentLatLngPosition, double currentYAW){
-        if(currentYAW < 0){
-            currentYAW += 180.0;
-        }
+        currentYAW = (-1.0) * currentYAW;
         currentYAW = currentYAW - 90.0;
 
-        double x = 0.01*Math.cos(Math.toRadians(currentYAW));
-        double y = 0.01*Math.sin(Math.toRadians(currentYAW));
+        double x = 0.003*Math.cos(Math.toRadians(currentYAW));
+        double y = 0.003*Math.sin(Math.toRadians(currentYAW));
         return new LatLng(currentLatLngPosition.latitude - y, currentLatLngPosition.longitude - x);
     }
 }
